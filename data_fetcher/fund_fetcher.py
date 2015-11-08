7ef77ba8a18574ab4f9a0d64eb536697917a4db1
icemoon1987 <panwenhai1987@163.com>
2015-11-08 21:02:48 +0800

    add fund_fetcher.py

--- data_fetcher/fund_fetcher.py
+++ data_fetcher/fund_fetcher.py
@@ -2,6 +2,7 @@
 #coding=utf-8
 
 import sys
+import re
 import urllib
 import myjson
 from datetime import datetime
@@ -18,34 +19,59 @@ class fund_fetcher(object):
         result = []
 
         try:
+            begindate_str = datetime.strptime(begindate, "%Y%m%d").strftime("%Y-%m-%d")
+            enddate_str = datetime.strptime(enddate, "%Y%m%d").strftime("%Y-%m-%d")
 
-            url = "http://biz.finance.sina.com.cn/fund/flash_hq/kline_data.php?symbol=%s&begin_date=%s&end_date=%s" % (fundid, begindate, enddate)
+            url = "http://fund.eastmoney.com/f10/F10DataApi.aspx?type=lsjz&code=%s&page=1&per=5000&sdate=%s&edate=%s&rt=0.7955276783627965"\
+                % (fundid, begindate_str, enddate_str)
+
+            print url
+
             data = urllib.urlopen(url).read()
-            tree = etree.HTML(data)
 
-            contents = tree.xpath("//control/content")
+            m = re.match(r'.*content:"(.*)"', data)
+            #m = re.match(r'.*', data)
+
+            html_data = m.group(1)
+
+            tree = etree.HTML(html_data)
+
+            contents = tree.xpath("//tbody/tr")
 
             for content in contents:
+
                 tmp = {}
-                date = content.xpath("./@d")[0]
-                open_price = content.xpath("./@o")[0]
-                high_price = content.xpath("./@h")[0]
-                close_price = content.xpath("./@c")[0]
-                low_price = content.xpath("./@l")[0]
-                volumn = content.xpath("./@v")[0]
+                date = content.xpath("./td/text()")[0]
+                close_price = float(content.xpath("./td/text()")[1])
+                acc_close_price = float(content.xpath("./td/text()")[2])
+                rise_rate = float(content.xpath("./td/text()")[3][:-1]) / 100
+                buy_status = content.xpath("./td/text()")[4]
+                sell_status = content.xpath("./td/text()")[5]
+                #meta = content.xpath("./td/text()")[6]
 
                 tmp["date"] = date
-                tmp["open_price"] = open_price
-                tmp["high_price"] = high_price
                 tmp["close_price"] = close_price
-                tmp["low_price"] = low_price
-                tmp["volumn"] = volumn
+                tmp["acc_close_price"] = acc_close_price
+                tmp["rise_rate"] = rise_rate
+                tmp["buy_status"] = buy_status
+                tmp["sell_status"] = sell_status
+                #tmp["meta"] = meta
 
                 result.append(tmp)
 
         except Exception, ex:
             print ex.__str__()
             return {}
+
+        return result
+
+
+    def post_process(self, data_list):
+        data_list.reverse()
+
+        for i in range(len(data_list)):
+            if i != 0:
+                data_list[i]["acc_close_price"] = data_list[i-1]["acc_close_price"] * (1 + data_list[i]["rise_rate"])
 
         return result
 
@@ -57,8 +83,8 @@ class fund_fetcher(object):
 
             for item in result:
                 f.write("%s,%s,%s,%s,%s,%s\n" % \
-                    (str(item["date"]), str(item["open_price"]), str(item["high_price"]), str(item["close_price"]),\
-                    str(item["low_price"]), str(item["volumn"])) )
+                    (str(item["date"]), "0", "0", str(item["acc_close_price"]),\
+                    "0", "0")) 
 
             f.close()
 
@@ -69,14 +95,17 @@ class fund_fetcher(object):
 
 if __name__ == "__main__":
 
-    """
     if len(sys.argv) >= 4:
         fetcher = fund_fetcher()
         result = fetcher.get_his_day_k(sys.argv[1], sys.argv[2], sys.argv[3])
+
+        print result
+
+        fetcher.post_process(result)
+
         print result
 
     if len(sys.argv) == 5:
         fetcher.store_to_file(result, sys.argv[4])
         print "store to file: " + sys.argv[1]
-    """
     
