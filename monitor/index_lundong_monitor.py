75ee8f11aba5185a2e6176f4ca10a264967ff8fe
manda.sun <manda.sun@qunar.com>
2016-08-09 19:41:40 +0800

    28轮动加入比较日期输出；更新历史数据文件；加入分级a折价率输出

--- monitor/index_lundong_monitor.py
+++ monitor/index_lundong_monitor.py
@@ -73,6 +73,10 @@ class index_lundong_monitor(object):
         result["sh000905"] = [sh000905_last.close_price, sh000905_now.close_price, sh000905_up]
         result["sz399008"] = [sz399008_last.close_price, sz399008_now.close_price, sz399008_up]
 
+        if sh000300_up < 0 and sh000905_up < 0 and sz399008_up < 0:
+            result["choise"] = "sell all"
+            return result
+
         if sh000300_up > sh000905_up and sh000300_up > sz399008_up:
             result["choise"] = "sh000300"
         elif sh000905_up > sh000300_up and sh000905_up > sz399008_up:
@@ -80,15 +84,12 @@ class index_lundong_monitor(object):
         elif sz399008_up > sh000300_up and sz399008_up > sh000905_up:
             result["choise"] = "sz399008"
 
-        if sh000300_up < 0 and sh000905_up < 0 and sz399008_up < 0:
-            result["result"] = "sell all"
-
         return result
 
     def format_result(self, result):
         delim = "        "
 
-        mail_detail = ""
+        mail_detail = "四周前日期： " + result["last_date"] + "\n"
         mail_detail += "市场类型" + delim + "指数名称" + delim + "指数代码" + delim + "四周前指数" + delim + "当前指数" + delim + "涨幅" + delim + "投资工具" + "\n"
         mail_detail += "权重盘" + delim + "沪深300" + delim + "sh000300" + delim + str(result["sh000300"][0]) + delim + str(result["sh000300"][1]) + \
                       delim + str(int(result["sh000300"][2] * 10000) / 100.0 ) + "%" + delim + "华泰柏瑞沪深300ETF(510300)、广发沪深300(270010)" + "\n"
@@ -114,27 +115,27 @@ class index_lundong_monitor(object):
 
 
     def format_html_result(self, result):
-
-        mail_detail = "<table border=\"1\"><tbody>"
+        mail_detail = "<p>四周前日期： " + result["last_date"] + "</p>\n"
+        mail_detail += "<table border=\"1\"><tbody>"
         mail_detail += "<tr><td>类型</td><td>名称</td><td>代码</td><td>四周前</td><td>当前</td><td>涨跌幅</td><td>工具</td></tr>\n"
 
         color = "green"
         if result["sh000300"][2] > 0:
             color = "red"
 
-        mail_detail += "<tr><td>权重</td><td>沪深300</td><td>sh000300</td><td>" + str(result["sh000300"][0]) + "</td><td>" + str(result["sh000300"][1]) + "</td><td><span style=\"color: " + color + ";\">" + str(int(result["sh000300"][2] * 10000) / 100.0 ) + "%" + "</span></td>" + "<td>华泰柏瑞沪深300ETF(510300)、广发沪深300(270010)</td>\n"
+        mail_detail += "<tr><td>权重</td><td>沪深300</td><td>sh000300</td><td>" + str(result["sh000300"][0]) + "</td><td>" + str(result["sh000300"][1]) + "</td><td><span style=\"color: " + color + ";\">" + str(int(result["sh000300"][2] * 10000) / 100.0 ) + "%" + "</span></td>" + "<td>华泰柏瑞沪深300ETF(510300)、广发沪深300(270010)</td></tr>\n"
 
         color = "green"
         if result["sh000905"][2] > 0:
             color = "red"
 
-        mail_detail += "<tr><td>中小</td><td>中小盘500</td><td>sh000905</td><td>" + str(result["sh000905"][0]) + "</td><td>" + str(result["sh000905"][1]) + "</td><td><span style=\"color: " + color + ";\">" + str(int(result["sh000905"][2] * 10000) / 100.0 ) + "%" + "</span></td>" + "<td>南方中证500ETF(510500)</td>\n"
+        mail_detail += "<tr><td>中小</td><td>中小盘500</td><td>sh000905</td><td>" + str(result["sh000905"][0]) + "</td><td>" + str(result["sh000905"][1]) + "</td><td><span style=\"color: " + color + ";\">" + str(int(result["sh000905"][2] * 10000) / 100.0 ) + "%" + "</span></td>" + "<td>南方中证500ETF(510500)</td></tr>\n"
 
         color = "green"
         if result["sz399008"][2] > 0:
             color = "red"
 
-        mail_detail += "<tr><td>中小</td><td>中小板300</td><td>sz399008</td><td>" + str(result["sz399008"][0]) + "</td><td>" + str(result["sz399008"][1]) + "</td><td><span style=\"color: " + color + ";\">" + str(int(result["sz399008"][2] * 10000) / 100.0 ) + "%" + "</span></td>" + "<td>广发中小板300联(270026)</td>\n"
+        mail_detail += "<tr><td>中小</td><td>中小板300</td><td>sz399008</td><td>" + str(result["sz399008"][0]) + "</td><td>" + str(result["sz399008"][1]) + "</td><td><span style=\"color: " + color + ";\">" + str(int(result["sz399008"][2] * 10000) / 100.0 ) + "%" + "</span></td>" + "<td>广发中小板300联(270026)</td></tr>\n"
 
         mail_detail += "</tbody></table>\n"
 
@@ -154,17 +155,18 @@ class index_lundong_monitor(object):
         return mail_detail
 
 if __name__ == '__main__':
+    mail_detail = "<h3>指数轮动模型结果：</h3>\n\n"
 
-    monitor = index_lundong_monitor()
-    result = monitor.monitor(27)
-
-    mail_detail = monitor.format_result(result)
+    index_monitor = index_lundong_monitor()
+    result = index_monitor.monitor(27)
+    mail_detail += index_monitor.format_html_result(result)
 
     mail_detail += "\n"
-    mail.sendmail(['546674175@qq.com', '182101630@qq.com', '81616822@qq.com', '373894584@qq.com'], "28轮动结果(潘文海)",mail_detail.encode("utf-8", "ignore"))
-    
-    mail_detail += "naopo我耐你哩~~~" + "\n"
-    mail.sendmail(['546674175@qq.com', '516563458@qq.com', 'sunada2005@163.com'], "28轮动结果(耐你滴老公~~)",mail_detail.encode("utf-8", "ignore"))
 
+    # mail.sendhtmlmail(['546674175@qq.com', '182101630@qq.com', '81616822@qq.com', '373894584@qq.com'], "28轮动结果(潘文海)",mail_detail.encode("utf-8", "ignore"))
+
+    mail_detail += "naopo我耐你哩~~~" + "\n"
+    # mail.sendhtmlmail(['546674175@qq.com', '516563458@qq.com', 'sunada2005@163.com'], "轮动模型结果(耐你滴老公)",mail_detail.encode("utf-8", "ignore"))
+    mail.sendhtmlmail(['sunada2005@163.com'], "轮动模型结果(耐你滴老公~)",mail_detail.encode("utf-8", "ignore"))
     print mail_detail
 
--- monitor/levela_lundong_monitor.py
+++ monitor/levela_lundong_monitor.py
@@ -84,6 +84,8 @@ class levela_lundong_monitor(object):
             line += "|" + str(item["price"])
             line += "|" + str(item["profit_rate"])
             line += "|" + str(item["descr"])
+            line += "|" + str(item["discount"])
+            # line += "|" + str(item[""])
             line += "\t"
 
         line += "\n"
@@ -107,7 +109,6 @@ class levela_lundong_monitor(object):
             item_list = []
 
             for item_str in ary[1:]:
-
                 item = {}
 
                 ary2 = item_str.split("|")
@@ -118,7 +119,7 @@ class levela_lundong_monitor(object):
                 item["price"] = ary2[3]
                 item["profit_rate"] = ary2[4]
                 item["descr"] = ary2[5]
-
+                item["discount"] = ary2[6]
                 item_list.append(item)
 
             results.append([date_str, item_list])
@@ -130,10 +131,10 @@ class levela_lundong_monitor(object):
         delim = "        "
 
         mail_detail = ""
-        mail_detail += "A基金代码" + delim + "A基金名称" + delim + "当前价格" + delim + "修正收益率" + "\n"
+        mail_detail += "A基金代码" + delim + "A基金名称" + delim + "当前价格" + delim + "修正收益率" + delim + "折价率" + "\n"
 
         for item in result:
-            mail_detail += item["id"] + delim + item["name"] + delim + item["price"] + delim + item["profit_rate"] + "\n"
+            mail_detail += item["id"] + delim + item["name"] + delim + item["price"] + delim + item["profit_rate"] + delim + item["discount"] + "\n"
 
         mail_detail += "\n"
 
@@ -144,19 +145,19 @@ class levela_lundong_monitor(object):
 
         mail_detail = "<p><strong>" + result["last_result"][0] + " 的结果：</strong></p>\n"
         mail_detail += "<table border=\"1\"><tbody>"
-        mail_detail += "<tr><td>代码</td><td>名称</td><td>当前价格</td><td>修正收益率</td>" + "\n"
+        mail_detail += "<tr><td>代码</td><td>名称</td><td>当前价格</td><td>修正收益率</td><td>折价率</td>" + "\n"
 
         for item in result["last_result"][1]:
-            mail_detail += "<tr><td>" + item["id"] + "</td><td>" + item["name"] + "</td><td>" + item["price"] + "</td><td>" + item["profit_rate"] + "</td></tr>\n"
+            mail_detail += "<tr><td>" + item["id"] + "</td><td>" + item["name"] + "</td><td>" + item["price"] + "</td><td>" + item["profit_rate"] + "</td><td>" + item["discount"] + "</td></tr>\n"
 
         mail_detail += "</tbody></table>\n"
 
         mail_detail += "<p><strong>今日结果：</strong></p>\n"
         mail_detail += "<table border=\"1\"><tbody>"
-        mail_detail += "<tr><td>代码</td><td>名称</td><td>当前价格</td><td>修正收益率</td>" + "\n"
+        mail_detail += "<tr><td>代码</td><td>名称</td><td>当前价格</td><td>修正收益率</td><td>折价率</td>" + "\n"
 
         for item in result["result"]:
-            mail_detail += "<tr><td>" + item["id"] + "</td><td>" + item["name"] + "</td><td>" + item["price"] + "</td><td>" + item["profit_rate"] + "</td></tr>\n"
+            mail_detail += "<tr><td>" + item["id"] + "</td><td>" + item["name"] + "</td><td>" + item["price"] + "</td><td>" + item["profit_rate"] + "</td><td>" + item["discount"] + "</td></tr>\n"
 
         mail_detail += "</tbody></table>\n"
         
@@ -197,9 +198,9 @@ if __name__ == '__main__':
         mail_detail = monitor.format_result(result)
 
         mail_detail += "\n"
-        #mail.sendmail(['546674175@qq.com', '182101630@qq.com', '81616822@qq.com', '373894584@qq.com'], "A级基金轮动结果(潘文海)",mail_detail.encode("utf-8", "ignore"))
+        #mail.sendhtmlmail(['546674175@qq.com', '182101630@qq.com', '81616822@qq.com', '373894584@qq.com'], "A级基金轮动结果(潘文海)",mail_detail.encode("utf-8", "ignore"))
     
         mail_detail += "naopo我耐你哩~~~" + "\n"
-        mail.sendmail(['546674175@qq.com', '516563458@qq.com', 'sunada2005@163.com'], "A级基金轮动结果(耐你滴老公~~)",mail_detail.encode("utf-8", "ignore"))
-
+        # mail.sendhtmlmail(['546674175@qq.com', '516563458@qq.com', 'sunada2005@163.com'], "A级基金轮动结果(耐你滴老公~~)",mail_detail.encode("utf-8", "ignore"))
+        mail.sendhtmlmail(['sunada2005@163.com'], "A级基金轮动结果(耐你滴老公~~)",mail_detail.encode("utf-8", "ignore"))
         print mail_detail
