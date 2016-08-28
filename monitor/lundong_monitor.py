510f377674468482a42415a15f50c7f140f4f299
manda.sun <manda.sun@qunar.com>
2016-08-28 23:08:28 +0800

    将海龟加入邮件内容

--- monitor/lundong_monitor.py
+++ monitor/lundong_monitor.py
@@ -8,6 +8,7 @@ from datetime import datetime, timedelta
 from index_lundong_monitor import index_lundong_monitor
 from levela_lundong_monitor import levela_lundong_monitor
 from intelligent_fixed_investment_monitor import  intelligent_fixed_investment_monitor
+from turtle_monitor import turtle_monitor
 
 reload(sys)
 sys.setdefaultencoding('utf-8')
@@ -32,6 +33,11 @@ if __name__ == '__main__':
     intelligent.calc_money_today("sz159915")
     mail_detail += intelligent.format_format_html_result()
 
+    mail_detail += "<h3>4、沪深300海龟轮动：</h3>"
+    tmonitor = turtle_monitor()
+    code = "sh000300"  #沪深300
+    result = tmonitor.monitor(code)
+    mail_detail += tmonitor.format_format_result(result)
     # mail.sendhtmlmail(['546674175@qq.com', '182101630@qq.com', '81616822@qq.com', '373894584@qq.com'], "轮动模型结果(潘文海)",mail_detail.encode("utf-8", "ignore"))
 
     mail.sendhtmlmail(['546674175@qq.com', '516563458@qq.com', 'sunada2005@163.com'], "轮动模型结果(耐你滴老公)",mail_detail.encode("utf-8", "ignore"))
--- monitor/turtle_monitor.py
+++ monitor/turtle_monitor.py
@@ -7,7 +7,7 @@ from data_fetcher.stock_fetcher import stock_fetcher
 from data_interface.stock_dataset import stock_dataset
 from data_interface.stock_data import stock_data
 from model.turtle import turtle
-from datetime import datetime
+from datetime import datetime,timedelta
 import mail
 from define import *
 
@@ -18,7 +18,8 @@ class turtle_monitor:
     def __init__(self):
         return
 
-    def monitor(self, code, filename, delim):
+    # def monitor(self, code, filename, delim):
+    def monitor(self, code):
         data_set = stock_dataset()
         data_set.load_from_file(code, "../data/" + code)
 
@@ -28,29 +29,36 @@ class turtle_monitor:
 
         # stock = stock_data()
         # stock.close_price = net
-        today = datetime.now().strftime("%Y-%m-%d")
+        # today = datetime.now().strftime("%Y-%m-%d")
+
+        # today = datetime.now() - timedelta(days = 2)  #周末测试时使用
+        today = datetime.now()
+        today = today.strftime("%Y-%m-%d")
+
         # stock.date = today
         # data_set.data.append(stock)
 
         turtle_plan3 = turtle()
         result = turtle_plan3.get_trading_plan3(data_set, today)
+        result["code"] = code
 
-        f = open(filename, "r+")
-        line = f.readline()
-        arr = line.strip().split(delim)
-        share = arr[2]
-        money = arr[3]
-        profit = arr[4]
-        if(result["choise"] == 4):  #buy
-            result["share"] = round((money * (1 - DEAL_RATE) / result["close_price"]), 3)
-            result["money"] = round(money * (1 - DEAL_RATE), 3)
-            result["profit"] = profit
-            self.append_result_to_file(filename, result, delim)
-        elif(result["choise"] == 2): #sell
-            result["share"] = share
-            result["money"] = round(result["close_price"] * share * (1 - DEAL_RATE), 3)
-            result["profit"] = result["money"] - money
-            self.append_result_to_file(filename, result, delim)
+        # f = open(filename, "r+")
+        # line = f.readline()
+        # arr = line.strip().split(delim)
+        # print "arr:", arr
+        # share = arr[2]
+        # money = arr[3]
+        # profit = arr[4]
+        # if(result["choise"] == 4):  #buy
+        #     result["share"] = round((money * (1 - DEAL_RATE) / result["close_price"]), 3)
+        #     result["money"] = round(money * (1 - DEAL_RATE), 3)
+        #     result["profit"] = profit
+        #     self.append_result_to_file(filename, result, delim)
+        # elif(result["choise"] == 2): #sell
+        #     result["share"] = share
+        #     result["money"] = round(result["close_price"] * share * (1 - DEAL_RATE), 3)
+        #     result["profit"] = result["money"] - money
+        #     self.append_result_to_file(filename, result, delim)
         return result
 
 
@@ -65,11 +73,21 @@ class turtle_monitor:
         f.write(line + content)
         f.close()
 
+    def format_format_result(self, result):
+        detail = "today: " + result["date"] + "<br/>code:" + result["code"] + "<br/>file_end_date:" + result["end_date"] + \
+                 "<br/>BUY_DAYS:" + result["BUY_DAYS"] + "<br/>SELL_DAYS:" + result["SELL_DAYS"] + \
+                 "<br/>start_buy_date:" + result["start_buy_date"] + "<br/>start_sell_date:" + result["start_sell_date"] + \
+                 "<br/>close_price: " + str(result["close_price"]) + "<br/>choise:" + result["info"] + "<br/>"
+        if(result["choise"] >= 0):
+            detail += "max_date: " + str(result["max_date"]) + "<br/>min_date:" + str(result["min_date"])
+        return detail
+
 
 if __name__ == "__main__":
     tmonitor = turtle_monitor()
+    code = "sh000300"  #沪深300
+    result = tmonitor.monitor(code, "../result/turtle3_result", "|")
 
-    result = tmonitor.monitor("sh000905", "../result/turtle3_result", "|")
     """
     choise:
         -3:	not enough data, do not trade
@@ -81,13 +99,8 @@ if __name__ == "__main__":
         3:	mean line unsatisfy, do not trade
         4:	buy
      """
-
-    detail = "today: " + result["date"] + "\nend_date:" + result["end_date"] + "\nstart_buy_date:" + \
-             result["start_buy_date"] + "\nstart_sell_date:" + result["start_sell_date"] + "\nclose_price: " + \
-             str(result["close_price"]) + "\n\nchoise:" + result["info"] + "\n\n"
-    if(result["choise"] >= 0):
-        detail += "max_date: " + str(result["max_date"]) + "\nmin_date:" + str(result["min_date"])
-
-    mail.sendmail(['sunada2005@163.com'], "轮动模型结果(耐你滴老公~)",detail.encode("utf-8", "ignore"))
+    detail = tmonitor.format_format_result(result)
+    #
+    # mail.sendmail(['sunada2005@163.com'], "轮动模型结果(耐你滴老公~)",detail.encode("utf-8", "ignore"))
     print detail
 
