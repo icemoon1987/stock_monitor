8d3c1a6b1bf75e98f70098e3f58eb317503b3ff7
manda.sun <manda.sun@qunar.com>
2016-09-03 20:29:58 +0800

    修改智能定投的定投额为第月3000元

--- define.py
+++ define.py
@@ -9,10 +9,10 @@ LOW_PRICE_POS = 4
 VOLUMN_POS = 5
 
 #价值平均法：https://www.jisilu.cn/question/61866  https://www.jisilu.cn/question/61196
-GAP = 4000
+GAP = 3000
 DEAL_RATE = 0.0002
 EXPE_RATE = 0.02
-MAX_GAP = 20000  #MAX_GAP为GAP的5倍时回测效果较好；每月定投额应在[-MAX_GAP, MAX_GAP]之间
+MAX_GAP = 15000  #MAX_GAP为GAP的5倍时回测效果较好；每月定投额应在[-MAX_GAP, MAX_GAP]之间
 
 #以天为交易单位的海龟 2016-9-1
 BUY_DAYS = 76
