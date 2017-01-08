bb53266fdeb1c81cbaa627fa87ebe225338fed50
sunada <sunada2005@163.com>
2017-01-08 22:53:31 +0800

    small market value, amount bigger than 10 billion, not *ST

---
+++ monitor/small_market_value.py
@@ -0,0 +1,13 @@
+import pandas
+import tushare as ts
+import re
+
+stRegex = re.compile(r"^[^\*ST.*]")
+
+lc = ts.get_today_all()
+# lc.to_csv('a.txt')
+# lc = pandas.read_csv('a.txt',encoding='gbk')
+lc_amount = lc.query('amount>10000000')
+lc_amount_except_ST = lc_amount[(lc_amount['name'].str.contains(stRegex, regex=True))]
+res = lc_amount_except_ST.sort_values(by="mktcap").head(10)
+print(res)
