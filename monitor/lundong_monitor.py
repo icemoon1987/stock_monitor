497fc08812725df3000a72215624e0b96b6ead0e
sunada <sunada2005@163.com>
2017-02-18 22:47:06 +0800

    优化：调整邮件中信息的位置

--- monitor/lundong_monitor.py
+++ monitor/lundong_monitor.py
@@ -15,22 +15,23 @@ sys.setdefaultencoding('utf-8')
 
 if __name__ == '__main__':
 
-    mail_detail = "<h3>1、28指数轮动模型结果（广发基金账户）：</h3>"
+    mail_detail = ""
 
-    index_monitor = index_lundong_monitor()
-    result = index_monitor.monitor(27)
-    mail_detail += index_monitor.format_html_result(result)
-    mail_detail += "\n"
+    mail_detail += "<h3>1、价值平均法定投（01证券账户）:</h3>"
+    intelligent = intelligent_fixed_investment_monitor()
+    intelligent.calc_money_today("sz159915")
+    mail_detail += intelligent.format_format_html_result()
 
     mail_detail += "<h3>2、分级A轮动模型结果（01证券账户）：</h3>"
     levela_monitor = levela_lundong_monitor()
     result = levela_monitor.monitor(5)
     mail_detail += levela_monitor.format_html_result(result)
 
-    mail_detail += "<h3>3、价值平均法定投（01证券账户）:</h3>"
-    intelligent = intelligent_fixed_investment_monitor()
-    intelligent.calc_money_today("sz159915")
-    mail_detail += intelligent.format_format_html_result()
+    mail_detail += "<h3>3、28指数轮动模型结果（广发基金账户）：</h3>"
+    index_monitor = index_lundong_monitor()
+    result = index_monitor.monitor(27)
+    mail_detail += index_monitor.format_html_result(result)
+    mail_detail += "\n"
 
     mail_detail += "<h3>4、沪深300海龟轮动（01证券账户）：</h3>"
     tmonitor = turtle_monitor()
